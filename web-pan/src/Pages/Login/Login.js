a1718b24f5b8d13eccf7e6ffd70517ccc5a3d694
huangtianrun <huangtianrun@jd.com>
2021-03-12 17:43:19 +0800

    feat：移动文件联调

--- web-pan/src/Pages/Login/Login.js
+++ web-pan/src/Pages/Login/Login.js
@@ -1,94 +1,111 @@
 import React, { useState } from "react";
 
-import { Input, Button, Form } from "antd";
+import { withRouter } from "react-router-dom";
+
+import {toJS} from 'mobx'
+import { Input, Button, Form, message } from "antd";
 import { observer, inject } from "mobx-react";
 import classnames from "classnames";
 
 import "./Login.scss";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default inject("login")(
-  observer(({ login: { loginFnc, registerFnc } }) => {
-    const [form] = Form.useForm();
-    const [select, setSelect] = useState(1);
+export default withRouter(
+  inject("login")(
+    observer(
+      ({ history, login: { loginFnc, registerFnc, update, userData } }) => {
+        const [form] = Form.useForm();
+        const [select, setSelect] = useState(1);
 
-    const loginOrRegistered = (i) => {
-      setSelect(i);
-    };
+        const loginOrRegistered = (i) => {
+          setSelect(i);
+        };
 
-    const onFinish = async (e) => {
-        if(select === 1) {
-            const data = await loginFnc(e.userId, e.passWord)  
-            console.log(data)
-        }else {
-            const data = await registerFnc(e.userId, e.userName, e.passWord)
-            console.log(data)
-        }
-    };
+        const onFinish = async (e) => {
+          if (select === 1) {
+            const data = await loginFnc(e.userId, e.passWord);
+            if (data.code === 200) {
+              message.success(data.msg);
+              await update({ userData: data.data });
+              console.log(toJS(userData))
+              history.push(
+                `/home?destFolderId=${userData.baseFolderId}&userId=${userData.user.userId}`
+              );
+            } else {
+              message.error(data.msg);
+            }
+          } else {
+            const data = await registerFnc(e.userId, e.userName, e.passWord);
+            if (data.code === 500) {
+              message.error("改手机号已被注册");
+              return;
+            } else {
+              message.success("注册成功，请登录");
+            }
+          }
+        };
 
-    return (
-      <div className="login">
-        <div className="content">
-          <div className="span-btn">
-            <span
-              onClick={() => {
-                loginOrRegistered(1);
-              }}
-              className={classnames({ selected: select === 1 })}
-            >
-              登录
-            </span>
-            <span className="point">.</span>
-            <span
-              onClick={() => {
-                loginOrRegistered(2);
-              }}
-              className={classnames({ selected: select === 2 })}
-            >
-              注册
-            </span>
-          </div>
-          <Form form={form} onFinish={onFinish}>
-            {select === 2 && (
-              <Form.Item
-                name="userName"
-                rules={[{ required: true, message: "请输入你的昵称" }]}
-              >
-                <Input className="input" placeholder="你的昵称" />
-              </Form.Item>
-            )}
-            <Form.Item
-              name="userId"
-              rules={[{ required: true, message: "请输入你的手机号" }]}
-            >
-              <Input className="input" placeholder="手机号" />
-            </Form.Item>
-            <Form.Item
-              name="passWord"
-              rules={[{ required: true, message: "请输入你的密码" }]}
-            >
-              <Input.Password className="input" placeholder="密码" />
-            </Form.Item>
-            {select === 1 ? (
-              <Form.Item
-                name="login"
-              >
-                <Button className="btn" type="primary" htmlType="submit">
+        return (
+          <div className="login">
+            <div className="content">
+              <div className="span-btn">
+                <span
+                  onClick={() => {
+                    loginOrRegistered(1);
+                  }}
+                  className={classnames({ selected: select === 1 })}
+                >
                   登录
-                </Button>
-              </Form.Item>
-            ) : (
-              <Form.Item
-                name="register"
-              >
-                <Button className="btn" type="primary" htmlType="submit">
+                </span>
+                <span className="point">.</span>
+                <span
+                  onClick={() => {
+                    loginOrRegistered(2);
+                  }}
+                  className={classnames({ selected: select === 2 })}
+                >
                   注册
-                </Button>
-              </Form.Item>
-            )}
-          </Form>
-        </div>
-      </div>
-    );
-  })
+                </span>
+              </div>
+              <Form form={form} onFinish={onFinish}>
+                {select === 2 && (
+                  <Form.Item
+                    name="userName"
+                    rules={[{ required: true, message: "请输入你的昵称" }]}
+                  >
+                    <Input className="input" placeholder="你的昵称" />
+                  </Form.Item>
+                )}
+                <Form.Item
+                  name="userId"
+                  rules={[{ required: true, message: "请输入你的手机号" }]}
+                >
+                  <Input className="input" placeholder="手机号" />
+                </Form.Item>
+                <Form.Item
+                  name="passWord"
+                  rules={[{ required: true, message: "请输入你的密码" }]}
+                >
+                  <Input.Password className="input" placeholder="密码" />
+                </Form.Item>
+                {select === 1 ? (
+                  <Form.Item name="login">
+                    <Button className="btn" type="primary" htmlType="submit">
+                      登录
+                    </Button>
+                  </Form.Item>
+                ) : (
+                  <Form.Item name="register">
+                    <Button className="btn" type="primary" htmlType="submit">
+                      注册
+                    </Button>
+                  </Form.Item>
+                )}
+              </Form>
+            </div>
+          </div>
+        );
+      }
+    )
+  )
 );
--- web-pan/src/Store/Login/index.js
+++ web-pan/src/Store/Login/index.js
@@ -1,5 +1,4 @@
-import { message } from "antd";
-import { action, extendObservable, runInAction } from "mobx";
+import { action, extendObservable } from "mobx";
 
 import * as api from '../../Service/Login';
 
@@ -13,24 +12,14 @@ class Login {
   }
 
   @action.bound loginFnc = async (userId, password) => { // 之前没有用箭头函数的方式能获取到this，不知道为什么这次不行
-    try {
-      const data = await api.login({ userId, password })
-      return data
-    } catch (err) {
-      message.error(err.msg || '出错啦~')
-    }
+      return await api.login({ userId, password })
   }
 
   @action.bound registerFnc = async (userId, username, password) => { // 之前没有用箭头函数的方式能获取到this，不知道为什么这次不行
-    try {
-      const data = await api.register({ userId, username, password })
-      return data
-    } catch (err) {
-      message.error(err.msg || '出错啦~')
-    }
+      return await api.register({ userId, username, password })
   }
   
-  @action.bound update(data) {
+  @action.bound update = (data) => {
     Object.assign(this, data);
   }
 }
